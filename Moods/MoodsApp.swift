a815d80df19bfeace8599b922e629f96c8fa152c
AdvaitThePro <advaitthepro@outlook.com>
2024-12-26 21:38:45 +0800

    Initial Commit

---
+++ Moods/MoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodsApp.swift
+//  Moods
+//
+//  Created by Milind Contractor on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
